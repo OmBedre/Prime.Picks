9f4f1ae6c55c819804e32a44125ece770f8d8820
OmBedre <131977679+OmBedre@users.noreply.github.com>
2024-07-08 01:51:02 +0530

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -134,5 +134,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 from  django.contrib import messages
 MESSAGE_TAGS = {
-    messages.ERROR : 'danger' 
+    messages.ERROR : 'danger',
+    
 }
